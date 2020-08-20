4e8bc4503ac78c0829855d6e3b6098a3e89050f4
Andreas Wijaya <andreas.wijaya@tokopedia.com>
2020-08-20 11:44:46 +0700

    Objective 4 completed

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 
 	// dTwitterHttp "github.com/jauntyjack/compfest/delivery/twitter/http"
 	dTwitterFlag "github.com/jauntyjack/compfest/delivery/twitter/flag"
-	rTwitterCli "github.com/jauntyjack/compfest/repository/twitter/cli"
+	// rTwitterCli "github.com/jauntyjack/compfest/repository/twitter/cli"
+	rTwitterFile "github.com/jauntyjack/compfest/repository/twitter/file"
 	uTwitterModule "github.com/jauntyjack/compfest/usecase/twitter/module"
 )
 
 func main() {
-	twitterCli := rTwitterCli.New()
-	twitterUsecase := uTwitterModule.New(twitterCli)
+	// twitterCli := rTwitterCli.New()
+	twitterFile := rTwitterFile.New()
+	twitterUsecase := uTwitterModule.New(twitterFile)
 	dTwitterFlag.New(twitterUsecase)
 
 	// log.Print("Listen to port 8090")
